d82b6a4e1562c0684e9e2e3e6ae6224e118f028f
eulersIDcrisis <asgibsonua@zoho.com>
2021-11-27 21:42:02 -0700

    Updating pyinstaller script.

---
+++ create_standalone_bundle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Run pyinstaller to create a bundle.
+#
+MAIN_SCRIPT="pysdrweb/server/main.py"
+CURR_DIR=`pwd`
+
+pyinstaller ${MAIN_SCRIPT} -n sdr_server -p ${CURR_DIR} --add-data "pysdrweb/static/*:pysdrweb/static"
+
