e0cfefa737aee32159851ce853132152dae485c7
Aaron Gibson <asgibson@asu.edu>
2021-12-03 19:28:37 -0800

    Include 'soundfile' import in install.

--- create_standalone_bundle.sh
+++ create_standalone_bundle.sh
@@ -5,5 +5,7 @@
 MAIN_SCRIPT="pysdrweb/server/main.py"
 CURR_DIR=`pwd`
 
-pyinstaller ${MAIN_SCRIPT} -n sdr_server -p ${CURR_DIR} --add-data "pysdrweb/static/*:pysdrweb/static"
+pyinstaller ${MAIN_SCRIPT} -n sdr_server -p ${CURR_DIR} \
+  --add-data "pysdrweb/static/*:pysdrweb/static" \
+  --hidden-import "soundfile"
 
