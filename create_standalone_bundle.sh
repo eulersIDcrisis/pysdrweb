a80f680ac26f118a418c4cf9d88b56115230872e
eulersIDcrisis <asgibsonua@zoho.com>
2021-12-21 18:31:21 -0700

    Fixing standalone bundle script with module move.

--- create_standalone_bundle.sh
+++ create_standalone_bundle.sh
@@ -2,7 +2,7 @@
 #
 # Run pyinstaller to create a bundle.
 #
-MAIN_SCRIPT="pysdrweb/server/main.py"
+MAIN_SCRIPT="pysdrweb/fmserver/server.py"
 CURR_DIR=`pwd`
 
 pyinstaller ${MAIN_SCRIPT} -n sdrfm_server -p ${CURR_DIR} \
