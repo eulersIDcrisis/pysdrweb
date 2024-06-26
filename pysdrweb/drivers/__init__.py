6ee16646c56f854fd5053aee8783e609d25e041c
Aaron Gibson <asgibsonua@zoho.com>
2024-06-25 21:33:35 -0700

    Moving drivers into a dedicated module.

---
+++ pysdrweb/drivers/__init__.py
@@ -0,0 +1,11 @@
+"""pysdrweb.drivers module.
+
+Module for basic drivers to the RTL system.
+"""
+from pysdrweb.drivers.base import AbstractRtlDriver
+from pysdrweb.drivers.rtl_fm import RtlFmExecDriver
+
+__all__ = [
+    "AbstractRtlDriver",
+    "RtlFmExecDriver",
+]
